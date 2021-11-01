267305910625b698401cf2a6b2b46f9b765bff22
William <william.gravina@gcbinvestimentos.com>
2021-11-01 18:00:55 -0300

    feat: created implementation for status config

---
+++ src/application/configs/status.config.ts
@@ -0,0 +1,10 @@
+import { IStatusConfig } from '@domain/configs/status.config';
+
+export const statusConfig = {
+  reviewer: {
+    pending: 'pending',
+  },
+  article: {
+    pending: 'pending',
+  },
+} as IStatusConfig;
