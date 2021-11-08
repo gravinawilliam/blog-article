7f736e2b80ef0e71143d89c7b54602cc10798294
William <william.gravina@gcbinvestimentos.com>
2021-11-07 23:23:41 -0300

    feat: created implementation for controller create reviewer

---
+++ src/application/controllers/reviewers/create-reviewer.controller.ts
@@ -0,0 +1,31 @@
+import { ICreateReviewerUseCase } from '@domain/use-cases/reviewers/create-reviewer.usecase';
+import { ICreateReviewerValidator } from '@domain/validators/reviewers/create-reviewer.validator';
+
+import { IController } from '@shared/interfaces/controller.interface';
+import { IHttpRequest } from '@shared/interfaces/http-request.interface';
+import { IHttpResponse } from '@shared/interfaces/http-response.interface';
+import { created } from '@shared/utils/http-response';
+
+export class CreateReviewerController implements IController {
+  constructor(
+    private readonly createReviewerUseCase: ICreateReviewerUseCase,
+    private readonly createReviewerValidator: ICreateReviewerValidator,
+  ) {}
+
+  async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const { authorization } = httpRequest.headers;
+
+    const reviewerValidated = await this.createReviewerValidator.execute({
+      authorization,
+    });
+    if (reviewerValidated.isLeft()) return reviewerValidated.value;
+
+    const { userId } = reviewerValidated.value;
+
+    const reviewer = await this.createReviewerUseCase.execute({
+      userId,
+    });
+
+    return created(reviewer);
+  }
+}
