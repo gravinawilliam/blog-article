9270676782dfe827f0d663e09312171b0750576f
William <william.gravina@gcbinvestimentos.com>
2021-11-07 23:48:37 -0300

    feat: created factory for create subject

---
+++ src/main/factories/subjects/create-subject.factory.ts
@@ -0,0 +1,35 @@
+import { CreateSubjectController } from '@application/controllers/subjects/create-subject.controller';
+import { CreateSubjectUseCase } from '@application/use-cases/subjects/create-subject.usecase';
+import { RequiredFieldsValidator } from '@application/validators/_shared/required-fields.validator';
+import { VerifyPermissionValidator } from '@application/validators/_shared/verify-permission.validator';
+import { CreateSubjectValidator } from '@application/validators/subjects/create-subject.validator';
+
+import { TokenJwtProvider } from '@infra/_providers/token-jwt/token-jwt.provider';
+import CoursesTypeormRepository from '@infra/database/typeorm/repositories/courses-typeorm.repository';
+import SubjectsTypeormRepository from '@infra/database/typeorm/repositories/subjects-typeorm.repository';
+import UsersTypeormRepository from '@infra/database/typeorm/repositories/users-typeorm.repository';
+
+import { IController } from '@shared/interfaces/controller.interface';
+
+export const makeCreateSubjectController = (): IController => {
+  const requiredFieldsValidator = new RequiredFieldsValidator();
+  const subjectsRepository = new SubjectsTypeormRepository();
+  const coursesRepository = new CoursesTypeormRepository();
+  const usersRepository = new UsersTypeormRepository();
+  const verifyPermissionValidator = new VerifyPermissionValidator(
+    usersRepository,
+  );
+  const tokenProvider = new TokenJwtProvider();
+  const createSubjectValidator = new CreateSubjectValidator(
+    requiredFieldsValidator,
+    coursesRepository,
+    subjectsRepository,
+    verifyPermissionValidator,
+    tokenProvider,
+  );
+  const createSubjectUseCase = new CreateSubjectUseCase(subjectsRepository);
+  return new CreateSubjectController(
+    createSubjectUseCase,
+    createSubjectValidator,
+  );
+};
