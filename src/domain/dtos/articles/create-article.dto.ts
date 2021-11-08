b2feaf02464a34a8b5f7aae226a6a5264f521048
William <william.gravina@gcbinvestimentos.com>
2021-11-07 23:04:06 -0300

    feat: created dto for create article

---
+++ src/domain/dtos/articles/create-article.dto.ts
@@ -0,0 +1,49 @@
+import { TopicModel } from '../../models/topic.model';
+
+export type IParamsCreateArticleValidatorDTO = {
+  title: string;
+  topicIds: string[];
+  authorization: string;
+  description: string;
+};
+
+export type IResponseCreateArticleValidatorDTO = {
+  userId: string;
+  topics: TopicModel[];
+};
+
+export type IParamsCreateArticleUseCaseDTO = {
+  authorId: string;
+  reviewerId: string;
+  title: string;
+  description: string;
+  topics: TopicModel[];
+};
+
+export type IResponseCreateArticleUseCaseDTO = {
+  id: string;
+  status: string;
+};
+
+export type IParamsCreateArticleRepositoryDTO = {
+  authorId: string;
+  reviewerId: string;
+  status: string;
+  title: string;
+  description: string;
+  topics: TopicModel[];
+};
+
+export type IParamsCreateArticleAfterUseCaseTransformerDTO = {
+  topicIds: string[];
+  articleId: string;
+};
+
+export type IResponseCreateArticleBeforeUseCaseTransformerDTO = {
+  reviewerId: string;
+};
+
+export type IParamsCreateArticleTopicRepositoryDTO = {
+  articleId: string;
+  topicId: string;
+};
