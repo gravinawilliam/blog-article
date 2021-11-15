a8e784cf6780e67f20c6b8a8aaaf73341fe15410
William Gravina <dev.gravinawilliam@gmail.com>
2021-11-15 15:12:48 -0300

    feat: added status reviewer aprroved in status config

--- src/application/configs/status.config.ts
+++ src/application/configs/status.config.ts
@@ -3,6 +3,7 @@ import { IStatusConfig } from '@domain/configs/status.config';
 export const statusConfig = {
   reviewer: {
     pending: 'pending',
+    approved: 'approved',
   },
   article: {
     pending: 'pending',
