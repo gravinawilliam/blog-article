2959f16deea5323d892e8c9d973088242d968a04
William Gravina <dev.gravinawilliam@gmail.com>
2021-11-17 09:27:31 -0300

    feat: added implementations in subjects typeorm repository

--- src/infra/database/typeorm/repositories/subjects-typeorm.repository.ts
+++ src/infra/database/typeorm/repositories/subjects-typeorm.repository.ts
@@ -3,8 +3,10 @@ import { getRepository, Repository } from 'typeorm';
 import { ICreateSubjectRepository } from '@domain/repositories/subjects/create-subject.repository';
 import { IFindAllSubjectsRepository } from '@domain/repositories/subjects/find-all-subjects.repository';
 import { IFindByNameSubjectsRepository } from '@domain/repositories/subjects/find-by-name-subjects.repository';
+import { IFindSubjectsByCourseIdRepository } from '@domain/repositories/subjects/find-subjects-by-course-id.repository';
 
 import { IRequestCreateSubjectUseCaseDTO } from '@dtos/subjects/create-subject.dto';
+import { FindSubjectsByCourseIdRepositoryDTO } from '@dtos/subjects/subjects-repository.dto';
 
 import { SubjectModel } from '@models/subject.model';
 
@@ -18,7 +20,8 @@ export default class SubjectsTypeormRepository
     ICreateSubjectRepository,
     IFindByNameSubjectsRepository,
     IFindAllSubjectsRepository,
-    IFindByIdSubjectsRepository
+    IFindByIdSubjectsRepository,
+    IFindSubjectsByCourseIdRepository
 {
   private ormRepository: Repository<SubjectEntity>;
 
@@ -36,6 +39,14 @@ export default class SubjectsTypeormRepository
 
   public async findAll(): Promise<SubjectModel[]> {
     return await this.ormRepository.find();
+  }
+
+  public async findByCourseId({
+    courseId,
+  }: FindSubjectsByCourseIdRepositoryDTO.Params): FindSubjectsByCourseIdRepositoryDTO.Result {
+    return await this.ormRepository.find({
+      where: { courseId },
+    });
   }
 
   public async findById(
